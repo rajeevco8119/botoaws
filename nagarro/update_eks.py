5a33c2fe7936b192f05f832566572787d1794385
rajeevco8119 <rajeev.co8119@gmail.com>
2022-09-16 13:51:39 +0530

    Create update_eks.py

---
+++ nagarro/update_eks.py
@@ -0,0 +1,49 @@
+import sys
+
+import boto3
+import json
+
+min_node_count = 0
+max_node_count = 1
+desired_node_count = 0
+clusterName = 'oss-stage'
+
+client = boto3.client('eks', aws_access_key_id='<access-id>',
+                      aws_secret_access_key='<secret-key>',
+                      aws_session_token="<session-token>",
+                      region_name='<region-name>')
+
+
+def describe_cluster(clusterName):
+    response = client.describe_cluster(
+        name=clusterName
+    )
+    return response['cluster']['arn']
+
+
+def get_flag():
+    flag = client.list_tags_for_resource(
+        resourceArn=describe_cluster(clusterName=clusterName)
+    )
+    skip_automatic_stop = flag['tags']['--skip-automatic-stop']
+    return skip_automatic_stop
+
+
+def update_cluster():
+    response = client.update_nodegroup_config(
+        clusterName=clusterName,
+        nodegroupName='oss-stage-ng',
+        scalingConfig={
+            'minSize': min_node_count,
+            'maxSize': max_node_count,
+            'desiredSize': desired_node_count
+        }
+    )
+    print(response['update']['status'])
+
+
+if str(get_flag()) in ['no', 'NO', 'No']:
+    print('Updating cluster...')
+    update_cluster()
+else:
+    print('Flag "Skip automatic stop" is set to True...Hence not updating cluster')

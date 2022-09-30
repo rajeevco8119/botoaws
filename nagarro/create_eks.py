5fb687d9fc02f391cb8d84c866e2f1521f1855e3
rajeevco8119 <rajeev.co8119@gmail.com>
2022-09-30 14:39:19 +0530

    Update create_eks.py

--- nagarro/create_eks.py
+++ nagarro/create_eks.py
@@ -4,11 +4,12 @@ import json
 AWS_ACCESS_KEY_ID = ""
 AWS_SECRET_ACCESS_KEY = ""
 AWS_SESSION_TOKEN = ""
+AWS_REGION = 'eu-west-1'
 
 client = boto3.client('eks', aws_access_key_id=AWS_ACCESS_KEY_ID,
                       aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
                       aws_session_token=AWS_SESSION_TOKEN,
-                      region_name='eu-west-1')
+                      region_name=AWS_REGION)
 
 
 def list_clusters():
@@ -17,9 +18,7 @@ def list_clusters():
 
 
 def describe_cluster(cluster):
-    response = client.describe_cluster(
-        name=cluster
-    )
+    response = client.describe_cluster(name=cluster)
     json_response = json.dumps(response, indent=4, sort_keys=True, default=str)
     print(json_response)
 
@@ -35,7 +34,7 @@ def get_subnets(vpcid):
     client = boto3.client('ec2', aws_access_key_id=AWS_ACCESS_KEY_ID,
                           aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
                           aws_session_token=AWS_SESSION_TOKEN,
-                          region_name='eu-west-1')
+                          region_name=AWS_REGION)
     vpcs = []
     subnets = []
     for vpc in range(len(client.describe_vpcs()['Vpcs'])):
@@ -54,10 +53,19 @@ def describe_security_group():
     client = boto3.client('ec2', aws_access_key_id=AWS_ACCESS_KEY_ID,
                           aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
                           aws_session_token=AWS_SESSION_TOKEN,
-                          region_name='eu-west-1')
+                          region_name=AWS_REGION)
     response = client.describe_security_groups()['SecurityGroups']
     response = json.dumps(response, indent=4, sort_keys=True, default=str)
     print(response)
+
+
+def get_launch_template(instanceid):
+    response = boto3.client('ec2', aws_access_key_id=AWS_ACCESS_KEY_ID,
+                            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                            aws_session_token=AWS_SESSION_TOKEN,
+                            region_name=AWS_REGION)
+    data = response.get_launch_template_data(InstanceId=instanceid)
+    print(json.dumps(data, indent=4, sort_keys=True, default=str))
 
 
 def createCluster(clustername, arn, subnetid, security_grp_id,
@@ -74,23 +82,53 @@ def createCluster(clustername, arn, subnetid, security_grp_id,
     )
     print(response)
 
+
 def delete_cluster(clusterName):
     response = client.delete_cluster(
         name=clusterName
     )
     print(response)
 
+
 # By default AWS managed policy- "AmazonEKSClusterPolicy" is assigned to role for which RoleARN is assigned
+
+def create_nodegroup(cluster, nodegroup, minsize, maxsize, desiredsize, subnets, noderole, launchtemplate_name,
+                     launchtemplate_id):
+    response = client.create_nodegroup(
+        clusterName=cluster,
+        nodegroupName=nodegroup,
+        scalingConfig={
+            'minSize': minsize,
+            'maxSize': maxsize,
+            'desiredSize': desiredsize
+        },
+        subnets=subnets,
+        nodeRole=noderole,
+        launchTemplate={
+            'id': launchtemplate_id
+        },
+        capacityType='ON_DEMAND'
+    )
+    print(json.dumps(response, indent=4, sort_keys=True, default=str))
+    return
 
 # list_nodegroups('mytestcluster')
 # describe_cluster('mytestcluster')
-# get_subnets(vpcname)
+# getIAMRoles()
+# get_subnets('vpc-0456d8c89e0facd9f')
 
 # list_clusters()
 # describe_cluster(cluster='oss-stage')
-# createCluster('test_cluster_boto3', 'arn',
-#               get_subnets('vpc-id'), ['sg-name'])
+# createCluster('test_cluster_boto3', 'arn:aws:iam::2425225233522:role/eksctl-oss-stage-cluster-ServiceRole-15XOY3YRCZQVU',
+#               get_subnets('vpc-0456d8c89e0facd9f'), ['sg-0dd6b8ace144b0daf'])
+
+# subnet_ng = ['subnet-05b3aa4eae881f072', 'subnet-00a4718fafab5626f']
+# 
+# create_nodegroup('test_cluster_boto3','test_cluster_boto3_ng',1,1,1,
+#                  subnet_ng,'arn:aws:iam::474551112000:role/eksctl-oss-stage-nodegroup-oss-st-NodeInstanceRole-VLIOH4NRUAAS',
+#                  'eksctl-oss-stage-nodegroup-oss-stage-ng','lt-0d8f877cc54a89f90')
 
 # describe_security_group()
-# delete_cluster(clusterName='oss-stage')
+# delete_cluster('test_cluster_boto3')
 
+# get_launch_template('i-0361768edb6fffbfb')
